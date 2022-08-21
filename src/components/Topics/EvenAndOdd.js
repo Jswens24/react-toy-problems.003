79257c185fcbe37678838f5a1d6c1fb5bc9c8c43
Jessica Swenson <jessicamswens@gmail.com>
2022-08-20 19:26:07 -0600

    working even and odd component

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,13 +2,39 @@ import React, { useState } from "react";
 
 const EvenAndOdd = () => {
 
+    const [evenArr, setEvenArr] = useState([]);
+    const [oddArr, setOddArr] = useState([]);
+    const [userInput, setUserInput] = useState('');
+
+    const inputHandler = (event) => {
+        setUserInput(event.target.value);
+    }
+
+    const clickHandler = () => {
+        const evens = [];
+        const odds = [];
+        let userStr = userInput.split(',');
+
+        for (let i = 0; i < userStr.length; i++) {
+            if (userStr[i] % 2 === 0) {
+                evens.push(+userStr[i]);
+            } else {
+                odds.push(+userStr[i]);
+            };
+        }
+        setEvenArr(evens);
+        setOddArr(odds);
+    }
+
+
+
     return (
         <div className="puzzleBox evenAndOddPB">
             <h4>Evens and Odds</h4>
-            <input className="inputLine" type='text' />
-            <button className="confirmationButton">Button</button>
-            <span className="resultsBox">span1</span>
-            <span className="resultsBox">span2</span>
+            <input className="inputLine" type='text' onChange={inputHandler} />
+            <button className="confirmationButton" onClick={clickHandler}>Button</button>
+            <span className="resultsBox">Evens: {JSON.stringify(evenArr)}</span>
+            <span className="resultsBox">Odds: {JSON.stringify(oddArr)}</span>
         </div>
     )
 };
