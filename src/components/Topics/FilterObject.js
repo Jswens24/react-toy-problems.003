87023ef635db4dbfcfe49b593a397353aed9ccec
Jessica Swenson <jessicamswens@gmail.com>
2022-08-21 17:31:45 -0600

    functioning filterObject

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FilterObject = () => {
+    const [unFilteredArr, setUnFilteredArr] = useState([{ "name": "Jimmy Joe", "title": "Hack0r", "age": 12 }, { "name": "Jeremy Schrader", "age": 24, "hairColor": "brown" }, { "name": "Carly Armstrong", "title": "CEO" }]);
+    const [userInput, setUserInput] = useState('');
+    const [filteredArr, setFilteredArr] = useState([]);
+
+    const inputChangeHandler = (event) => {
+        setUserInput(event.target.value);
+    };
+
+    const buttonHandler = () => {
+        let resultArr = [];
+        unFilteredArr.filter(word => {
+            word.hasOwnProperty(userInput) && resultArr.push(word)
+        });
+        setFilteredArr(resultArr);
+    };
 
     return (
-        <p>FilterObject</p>
+        <div className='puzzleBox filterObjectPB'>
+            <h4>Filter Object</h4>
+            <span className='puzzleText'>{JSON.stringify(unFilteredArr)}</span>
+            <input onChange={inputChangeHandler} className='inputLine' type='text' />
+            <button onClick={buttonHandler} className='confirmationButton'>Button</button>
+            <span className='resultsBox filterObjectRB'>{JSON.stringify(filteredArr)}</span>
+        </div>
     )
 };
 
