b9b2dfed202bf23acd30c1b4c2bfce3cbd522036
Jessica Swenson <jessicamswens@gmail.com>
2022-08-22 18:54:58 -0600

    completed sum toy problem

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sum = () => {
+    const [numberOne, setNumberOne] = useState(0);
+    const [numberTwo, setNumberTwo] = useState(0);
+    const [sum, setSum] = useState(null);
+
+    const firstInputHandler = (event) => {
+        setNumberOne(event.target.value);
+    };
+
+    const secondInputHandler = (event) => {
+        setNumberTwo(event.target.value);
+    };
+
+    const clickHandler = () => {
+        const resultSum = +numberOne + +numberTwo;
+        setSum(resultSum);
+    }
+
 
     return (
-        <div className='puzzleBox sumP'>
+        <div className='puzzleBox sumPB'>
             <h4>Sum</h4>
-            <input type='text' />
-            <input type='text' />
-            <button>Button</button>
-            <span></span>
+            <input onChange={firstInputHandler} className='inputLine' type='text' />
+            <input onChange={secondInputHandler} className='inputLine' type='text' />
+            <button onClick={clickHandler} className='confirmationButton'>Button</button>
+            <span className='resultsBox'>{sum}</span>
         </div>
     )
 };
